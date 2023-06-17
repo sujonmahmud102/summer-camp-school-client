870252cfb64bafd2e2df473cb90563a7cec9e652
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-06-17 09:55:11 +0600

    dark light mode added

--- src/Pages/Home/Home/PopularInstructor/PopularInstrutor.jsx
+++ src/Pages/Home/Home/PopularInstructor/PopularInstrutor.jsx
@@ -15,17 +15,17 @@ const PopularInstrutor = () => {
 
             <div className='grid lg:grid-cols-3 gap-4 p-12'>
                 {
-                    instructors.map((instructor, index) => <div key={index} className="card w-full bg-base-100 shadow-xl border hover:border-orange-950">
-                        <figure className="px-10 pt-10"><img className='w-36 h-36 rounded-xl' src={instructor.image} alt="" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{instructor.name}</h2>
-                            <div className='flex justify-between'>
-                                <p>Email: {instructor.email}</p>
+                    instructors.slice(0, 6).map((instructor, index) => (
+                        <div key={index} className="card w-full bg-base-100 shadow-xl border hover:border-orange-950">
+                            <figure className="px-10 pt-10"><img className='w-36 h-36 rounded-xl' src={instructor.image} alt="" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{instructor.name}</h2>
+                                <div className='flex justify-between'>
+                                    <p>Email: {instructor.email}</p>
+                                </div>
                             </div>
-                            
                         </div>
-                    </div>
-                    )
+                    ))
                 }
             </div>
 
--- src/Pages/Home/WhyChooseUs/WhyChooseUs.jsx
+++ src/Pages/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -1,4 +1,6 @@
 import { BiStopwatch, BiBulb, BiExclude } from "react-icons/bi";
+import { Fade } from "react-awesome-reveal";
+
 
 const WhyChooseUs = () => {
     return (
@@ -12,9 +14,12 @@ const WhyChooseUs = () => {
                         <BiStopwatch></BiStopwatch>
                     </div>
                     <h3 className='mb-3 text-2xl hover:text-primary'>Self Reliant Individuals</h3>
-                    <p className='text-sm'>
-                        Our talent search methods encompass a range of strategies, including scouting local and regional competitions, partnering with schools and sports clubs, and conducting talent identification camps. We actively engage with coaches, physical education teachers, and community leaders to identify individuals who demonstrate exceptional athletic skills and a passion for their chosen sport.
-                    </p>
+                    <Fade>
+                        <p className='text-sm'>
+                            Our talent search methods encompass a range of strategies, including scouting local and regional competitions, partnering with schools and sports clubs, and conducting talent identification camps. We actively engage with coaches, physical education teachers, and community leaders to identify individuals who demonstrate exceptional athletic skills and a passion for their chosen sport.
+                        </p>
+                    </Fade>
+
                 </div>
 
                 <div className='border hover:border-red-400 text-center w-2/5 h-80 p-8 rounded-md bg-white'>
@@ -22,9 +27,12 @@ const WhyChooseUs = () => {
                         <BiBulb></BiBulb>
                     </div>
                     <h3 className='mb-3 text-2xl hover:text-error'>Talent Search Methods</h3>
-                    <p className='text-sm'>
-                        At our sports summer camp, we believe in nurturing self-reliant individuals who are equipped with the skills and mindset to succeed both on and off the field. We understand the importance of instilling independence and self-sufficiency in our campers. Through engaging activities and empowering experiences, we encourage campers to take ownership of their learning, develop problem-solving abilities, and build confidence in their abilities.
-                    </p>
+                    <Fade>
+                        <p className='text-sm'>
+                            At our sports summer camp, we believe in nurturing self-reliant individuals who are equipped with the skills and mindset to succeed both on and off the field. We understand the importance of instilling independence and self-sufficiency in our campers. Through engaging activities and empowering experiences, we encourage campers to take ownership of their learning, develop problem-solving abilities, and build confidence in their abilities.
+                        </p>
+                    </Fade>
+
                 </div>
 
 
@@ -33,9 +41,12 @@ const WhyChooseUs = () => {
                         <BiExclude></BiExclude>
                     </div>
                     <h3 className='mb-3 text-2xl hover:text-green-400'>Value Based Education</h3>
-                    <p className='text-sm'>
-                        Through a holistic approach, we integrate value-based education into every aspect of our program. Our experienced coaches and mentors serve as role models, demonstrating and teaching the importance of fair play, respect, and sportsmanship. We create a supportive and inclusive environment where campers learn to value diversity, empathy, and cooperation.
-                    </p>
+                    <Fade>
+                        <p className='text-sm'>
+                            Through a holistic approach, we integrate value-based education into every aspect of our program. Our experienced coaches and mentors serve as role models, demonstrating and teaching the importance of fair play, respect, and sportsmanship. We create a supportive and inclusive environment where campers learn to value diversity, empathy, and cooperation.
+                        </p>
+                    </Fade>
+
                 </div>
 
             </div>
